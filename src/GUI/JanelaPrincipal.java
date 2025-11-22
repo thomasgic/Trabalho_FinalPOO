9c57e07128f3ae9d958249f6b6f16d0777c73f66
thomasgic <thomasgic146@gmail.com>
2025-11-22 12:04:46 -0300

    tebase da tela de popções criada (sem tratamento de eventos)

--- src/GUI/JanelaPrincipal.java
+++ src/GUI/JanelaPrincipal.java
@@ -5,16 +5,19 @@ import java.awt.*;
 
 public class JanelaPrincipal extends JFrame {
 PainelFornecedor painelFornecedor;
+PainelInicial painelInicial;
 
     public JanelaPrincipal(){
         super();
         painelFornecedor = new PainelFornecedor();
+        painelInicial = new PainelInicial();
         JPanel PainelPrincipal = new JPanel();
         BorderLayout layoutPrincipal = new BorderLayout();
         setLayout(layoutPrincipal);
         this.setSize(800,350);
-        this.setTitle("Cadastro Fornecedor");
-        this.add(painelFornecedor.getPainel());
+        this.setTitle("Tela de opções");
+        //this.add(painelFornecedor.getPainel());
+        this.add(painelInicial.getPainelInicial());
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         setVisible(true);
--- src/GUI/PainelInicial.java
+++ src/GUI/PainelInicial.java
@@ -1,4 +1,24 @@
 package GUI;
 
+import javax.swing.*;
+
 public class PainelInicial {
+    private JLabel instrucaoLabel;
+    private JButton removeVendaButton;
+    private JButton finalizarButton;
+    private JButton consultaButton;
+    private JButton cadastroButton;
+    private JButton relatorioButton;
+    private JButton alterarDadosButton;
+    private JButton carregaDadosButton;
+    private JButton salvaButton;
+    private JPanel painelInicial;
+
+    public PainelInicial(){
+
+    }
+
+    public JPanel getPainelInicial(){
+        return painelInicial;
+    }
 }
